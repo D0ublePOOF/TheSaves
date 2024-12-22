03d5defb17a2c71a89a461a6d1508254df61af16
or '1' = '1 <162588960+D0ublePOOF@users.noreply.github.com>
2024-12-23 00:09:15 +0800

    Delete src directory

--- src/main/java/doublepoof/thesaves/UtilClasses/Worlds.java
+++
@@ -1,9 +0,0 @@
-package doublepoof.thesaves.UtilClasses;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Worlds {
-    public static List<String> save_illegalWorlds = new ArrayList<>();
-    public static List<String> load_illegalWorlds = new ArrayList<>();
-}
